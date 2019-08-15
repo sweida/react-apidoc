65108250adda4a56fb847748281a09d54301f54a
sweida <weidaxyy@163.com>
2019-08-16 01:15:07 +0800

    安装redux和开发工具

--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@/style/main.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import reducer from './reducers'
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <App />, document.getElementById('root') );
+const store = createStore(reducer, composeWithDevTools(
+    applyMiddleware(logger)
+))
 
-if (module.hot) {
-    module.hot.accept('./App', () => {
-        ReactDOM.render(<App />, document.getElementById('root'))
-    })
-}
+
+ReactDOM.render( 
+    <Provider store={ store }>
+        <App />, 
+    </Provider>,
+    document.getElementById('root') 
+);
+
+// if (module.hot) {
+//     module.hot.accept('./App', () => {
+//         ReactDOM.render(<App />, document.getElementById('root'))
+//     })
+// }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
