c201ea051698a304c50f485281df42f2e8b0795c
sweida <weidaxyy@163.com>
2019-09-04 22:19:53 +0800

    打包

---
+++ precache-manifest.3638135bba06fd11474d88a0c2b79d0a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d954441f60680b9e093cfa8f127a89",
+    "url": "./index.html"
+  },
+  {
+    "revision": "029eb4a37eab7aae38df",
+    "url": "./static/css/2.1d5ea050.chunk.css"
+  },
+  {
+    "revision": "e185ab4b73215b0b8ef2",
+    "url": "./static/css/main.0def712a.chunk.css"
+  },
+  {
+    "revision": "029eb4a37eab7aae38df",
+    "url": "./static/js/2.6a230131.chunk.js"
+  },
+  {
+    "revision": "e185ab4b73215b0b8ef2",
+    "url": "./static/js/main.d5c03e1a.chunk.js"
+  },
+  {
+    "revision": "bed71cec401e0feee6d3",
+    "url": "./static/js/runtime~main.c3eda72e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "./static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "./static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "./static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "./static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "./static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "./static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "./static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
--- src/store/connect.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import actions from 'actions/actionCreators.js'
-
-export default connect(
-    state => state.main,
-    dispatch => bindActionCreators(actions, dispatch)
-)
